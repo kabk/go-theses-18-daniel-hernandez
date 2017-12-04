33567759115e8c37d34c2cfa63cec2f27ba11097
Daniel Hernández Chacón <dhdzch@gmail.com>
2017-12-04 01:36:23 +0100

    added color diff function

--- build/js/main.js
+++ build/js/main.js
@@ -4,10 +4,40 @@ var fonts = ["sans-serif", "serif", "monospace"];
 var weights = ["normal", "bold", "bolder", "lighter"];
 var tTransforms = ["none", "capitalize", "uppercase", "lowercase"];
 var tDecorations = ["none", "underline", "none", "none", "none"];
+var c1 = []; //bg
+var c2 = []; //fg
 
 function rand(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+var colorDiff = function colorDiff(v1, v2) {
+	var i = void 0,
+	    d = 0;
+
+	for (i = 0; i < v1.length; i++) {
+		d += (v1[i] - v2[i]) * (v1[i] - v2[i]);
+	}
+
+	return Math.floor(Math.sqrt(d));
+};
+
+var applyColor = function applyColor() {
+	$("body").css("background-color", "rgb(" + c1[0] + "," + c1[1] + "," + c1[2] + ")");
+	$("body, a").css("color", "rgb(" + c2[0] + "," + c2[1] + "," + c2[2] + ")");
+};
+
+var randomColor = function randomColor() {
+	c1 = [rand(0, 255), rand(0, 255), rand(0, 255)];
+	c2 = [rand(0, 255), rand(0, 255), rand(0, 255)];
+
+	if (colorDiff(c1, c2) >= 140) {
+		applyColor();
+	} else {
+		console.log(colorDiff(c1, c2), "another color");
+		randomColor();
+	}
+};
 
 var activateProperty = function activateProperty(target, property, setting) {
 	if (rand(0, 1) === 1) {
@@ -16,30 +46,29 @@ var activateProperty = function activateProperty(target, property, setting) {
 };
 
 var design = function design() {
+
 	var colWidth = rand(30, 70);
-	console.log(colWidth + " colWidth");
+
 	$("#textWrap").css("width", colWidth + "%");
 	$("#images").css("width", 100 - colWidth + "%");
 	if (rand(0, 1) === 1) {
 		$("#textWrap").css("float", "left");
 		$("#images").css("float", "right");
-		console.log("1,2");
 	} else {
 		$("#textWrap").css("float", "right");
 		$("#images").css("float", "left");
-		console.log("2,1");
 	}
 
-	// activateProperty("body", "background-color", `rgba(${rand(0,255)},${rand(0,255)},${rand(0,255)},${rand(0,255)})`);
-	activateProperty("body, a", "color", "rgba(" + rand(0, 255) + "," + rand(0, 255) + "," + rand(0, 255) + "," + rand(0, 255) + ")");
 	activateProperty("body", "font-family", fonts[rand(0, fonts.length)]);
 	activateProperty("h3", "font-family", fonts[rand(0, fonts.length)]);
-	activateProperty("p", "font-size", rand(8, 18) + "pt");
-	activateProperty("h3", "font-size", rand(8, 40) + "pt");
+	activateProperty("p", "font-size", rand(10, 18) + "pt");
+	activateProperty("h3", "font-size", rand(10, 40) + "pt");
 	activateProperty("h3", "font-weight", weights[rand(0, weights.length)]);
 	activateProperty("h3", "text-transform", tTransforms[rand(0, tTransforms.length)]);
 	activateProperty("h3", "text-decoration", tDecorations[rand(0, tDecorations.length)]);
 	activateProperty("section, header, #images", "padding", rand(0, 24) + "px");
+
+	randomColor();
 };
 
 var time = 0;
--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,39 @@ const fonts = ["sans-serif","serif","monospace"];
 const weights = ["normal", "bold", "bolder", "lighter"];
 const tTransforms = ["none","capitalize","uppercase","lowercase"];
 const tDecorations = ["none","underline","none","none","none"];
-
+let c1 = []; //bg
+let c2 = []; //fg
 
 function rand(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+const colorDiff = (v1,v2) => {
+	let i,
+        d = 0;
+
+        for (i = 0; i < v1.length; i++) {
+            d += (v1[i] - v2[i])*(v1[i] - v2[i]);
+        }
+
+        return Math.floor(Math.sqrt(d));
+}
+
+const applyColor = () => {
+	$("body").css("background-color", `rgb(${c1[0]},${c1[1]},${c1[2]})`);
+	$("body, a").css("color", `rgb(${c2[0]},${c2[1]},${c2[2]})`);
+}
+
+const randomColor = () => {
+	c1 = [rand(0,255), rand(0,255), rand(0,255)];
+	c2 = [rand(0,255), rand(0,255), rand(0,255)];
+
+	if (colorDiff(c1,c2) >= 140) {
+		applyColor();
+	} else {
+		console.log(colorDiff(c1,c2),"another color",);
+		randomColor();
+	}
 }
 
 const activateProperty = (target, property, setting) => {
@@ -15,30 +44,31 @@ const activateProperty = (target, property, setting) => {
 }
 
 const design = () => {
-	var colWidth = rand(30,70);
-	console.log(colWidth+" colWidth");
+
+	let colWidth = rand(30,70);
+
+	
+	
 	$("#textWrap").css("width", colWidth+"%");
 	$("#images").css("width", 100-colWidth+"%");
 	if (rand(0,1)===1) {
 		$("#textWrap").css("float","left");
 		$("#images").css("float","right");
-		console.log("1,2");
 	} else {
 		$("#textWrap").css("float","right");
 		$("#images").css("float","left");
-		console.log("2,1");
 	}
 
-	// activateProperty("body", "background-color", `rgba(${rand(0,255)},${rand(0,255)},${rand(0,255)},${rand(0,255)})`);
-	activateProperty("body, a", "color", `rgba(${rand(0,255)},${rand(0,255)},${rand(0,255)},${rand(0,255)})`);
 	activateProperty("body", "font-family", fonts[rand(0,fonts.length)]);
 	activateProperty("h3", "font-family", fonts[rand(0,fonts.length)]);
-	activateProperty("p", "font-size", rand(8,18)+"pt");
-	activateProperty("h3", "font-size", rand(8,40)+"pt");
+	activateProperty("p", "font-size", rand(10,18)+"pt");
+	activateProperty("h3", "font-size", rand(10,40)+"pt");
 	activateProperty("h3", "font-weight", weights[rand(0,weights.length)]);
 	activateProperty("h3", "text-transform", tTransforms[rand(0,tTransforms.length)]);
 	activateProperty("h3", "text-decoration", tDecorations[rand(0,tDecorations.length)]);
 	activateProperty("section, header, #images", "padding", rand(0,24)+"px");
+
+	randomColor();
 }
 
 let time = 0;
